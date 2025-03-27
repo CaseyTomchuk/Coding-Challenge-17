6d4bfe6d95f1663958a48053bc699c4447f6c19e
Casey Tomchuk <Caseytomchuk@gmail.com>
2025-03-27 16:53:01 -0400

    Task 2: Create a SalesRep Class

--- cc_17.js
+++ cc_17.js
@@ -20,8 +20,35 @@ class Customer {
 };
 
 const firstCustomer = new Customer("casey", "caseytomchuk@gmail.com");
+const secondCustomer = new Customer ("albert", "albert@albertmail.com");
 firstCustomer.addPurchase(50);
 firstCustomer.addPurchase(60);
 
 console.log(firstCustomer);
 console.log(`Total Spent: $${firstCustomer.getTotalSpent()}`);
+
+// Task 2: Create a SalesRep Class
+
+class SalesRep {
+    constructor(name) {
+        this.name = name;
+        this.clients = [];
+    }
+
+    addClient(customerObject) {
+        this.clients.push(customerObject)
+    }
+
+    // Finding customer who's name matches the name parameter, then we are running getTotalSpent() for that client.
+    getClientTotal(name1) {
+        const client = this.clients.find(customer => customer.name === name1);
+        return client.getTotalSpent()
+    }
+};
+
+// Creating a sales rep and running both of its methods 
+const salesRep1 = new SalesRep("Elise Norman");
+salesRep1.addClient(firstCustomer);
+salesRep1.addClient(secondCustomer);
+console.log(`Sales Rep Clients: ${salesRep1.clients}`);
+console.log(`Total Spent for Client: $${salesRep1.getClientTotal("casey")}`);
